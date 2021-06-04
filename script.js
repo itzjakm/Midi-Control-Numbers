7bd175876eeeb52c13a69ac1a3b93c2a41876f2b
itzjakm <itzjak.moss@gmail.com>
2021-06-03 22:28:42 -0500

    Last 3 notes

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 WebMidi.enable(function (err) {
+  const lastNote = document.querySelector('.last-note');
+  const lastNote2 = document.querySelector('.last2-note');
+  const lastNote3 = document.querySelector('.last3-note');
   const output = WebMidi.getOutputByName('Clavinova');
   const input = WebMidi.getInputByName('Clavinova');
   input.addListener('controlchange', 1, function (e) {
     if (!(e.controller.name === 'sustenutopedal' && e.value === 127)) return;
     document.querySelector('.number').textContent++;
-    output.playNote('C3', 'all', { duration: 100 });
   });
-
+  input.addListener('noteon', 1, e => {
+    lastNote3.innerText = lastNote2.innerText;
+    lastNote2.innerText = lastNote.innerText;
+    lastNote.innerText = e.note.name;
+  });
   /*   input.addListener('noteon', 'all', function (e) {
    
     if (e.note.number === 36) document.querySelector('.number').textContent++;
