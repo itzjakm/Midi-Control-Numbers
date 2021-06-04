b48ab16c80c3fe0dcb12b8642a30695d50abc3fd
itzjakm <itzjak.moss@gmail.com>
2021-06-04 15:59:01 -0500

    Fix error if not midi device(only works in Clavinova)

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ WebMidi.enable(function (err) {
   const lastNote = document.querySelector('.last-note');
   const lastNote2 = document.querySelector('.last2-note');
   const lastNote3 = document.querySelector('.last3-note');
-  const output = WebMidi.getOutputByName('Clavinova');
+  // const output = WebMidi.getOutputByName('Clavinova');
   const input = WebMidi.getInputByName('Clavinova');
+  if (!input) return;
   input.addListener('controlchange', 1, function (e) {
     if (!(e.controller.name === 'sustenutopedal' && e.value === 127)) return;
     document.querySelector('.number').textContent++;
